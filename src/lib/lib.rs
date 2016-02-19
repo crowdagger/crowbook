ad16db489a33c7c41e8bd8741b83cfbef2e26fd5
Elisabeth Henry <liz.henry@ouvaton.org>
2016-02-19 03:01:21 +0100

    forgot to add lib.rs in previous commit (thanks Travis for telling me)

--- src/lib/lib.rs
+++ src/lib/lib.rs
@@ -1,5 +1,8 @@
 extern crate pulldown_cmark as cmark;
 extern crate mustache;
+extern crate zip;
+extern crate chrono;
+extern crate uuid;
 
 pub mod escape;
 pub mod parser;
@@ -9,6 +12,7 @@ pub mod cleaner;
 pub mod token;
 pub mod error;
 pub mod book;
+pub mod epub;
 
 pub use html::HtmlRenderer;
 pub use parser::Parser;
@@ -17,3 +21,4 @@ pub use cleaner::Cleaner;
 pub use cleaner::French;
 pub use error::{Result, Error};
 pub use book::Book;
+pub use epub::EpubRenderer;
