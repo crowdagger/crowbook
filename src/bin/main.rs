763c3671e80bec1d98a3304dfa840d08541f771d
Elisabeth Henry <liz.henry@ouvaton.org>
2016-02-20 05:49:41 +0100

    started using clap to parse arguments

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,24 +1,33 @@
 extern crate crowbook;
+extern crate clap;
 
 use crowbook::{Book};
 use std::env;
+use clap::{App,Arg};
 
 fn main() {
-    let mut args = env::args();
-    args.next(); //discard program name
+    let mut app = App::new("crowbook")
+        .version(env!("CARGO_PKG_VERSION"))
+        .author("Élisabeth Henry")
+        .about("Render a markdown book in Epub, PDF or HTML")
+        .arg_from_usage("-v, --verbose 'Activate verbose mode'")
+        .arg_from_usage("-o, --output=[FILE] 'Specifies an output file'")
+        .arg(Arg::with_name("book")
+             .index(1)
+             .required(true)
+             .help("A file containing the book configuration"));
 
-    match args.next() {
-        None => println!("Needs the name of a book config file"),
-        Some(ref s) => {
-            match Book::new_from_file(s) {
-                Ok(book) => {
-                    if let Err(err) = book.render_all()  {
-                        println!("{}", err);
-                    }
-                }
-                Err(err) => {
+    let matches = app.get_matches();
+
+    if let Some(s) = matches.value_of("book") {
+        match Book::new_from_file(s) {
+            Ok(book) => {
+                if let Err(err) = book.render_all()  {
                     println!("{}", err);
                 }
+            }
+            Err(err) => {
+                println!("{}", err);
             }
         }
     }
