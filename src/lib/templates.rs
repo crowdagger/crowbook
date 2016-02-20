0c97fde591dcb9510a40bfe83711a87a7130d993
Elisabeth Henry <liz.henry@ouvaton.org>
2016-02-20 18:44:48 +0100

    Changed license to LGPL

--- src/lib/templates.rs
+++ src/lib/templates.rs
@@ -1,3 +1,20 @@
+// Copyright (C) 2016 Élisabeth HENRY.
+//
+// This file is part of Crowbook.
+//
+// Crowbook is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// Caribon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received ba copy of the GNU Lesser General Public License
+// along with Crowbook.  If not, see <http://www.gnu.org/licenses/>.
+
 pub mod html {
     pub static TEMPLATE:&'static str = include_str!("../../templates/template.html");
     pub static CSS:&'static str = include_str!("../../templates/epub/stylesheet.css");
@@ -24,5 +41,5 @@ pub mod epub3 {
     pub static COVER:&'static str = include_str!("../../templates/epub3/cover.xhtml");
     pub static NAV:&'static str = include_str!("../../templates/epub3/nav.xhtml");
     pub static OPF:&'static str = include_str!("../../templates/epub3/content.opf");
-    pub static TITLE:&'static str = include_str!("../../templates/epub3/titlepage.xhtml");
+    pub static TITLE:&'static str = include_str!("../../templates/epub3</titlepage.xhtml");
 }
