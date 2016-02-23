a728f466e624678dfd3b8ff7bfbeaa19ea1050e5
Elisabeth Henry <liz.henry@ouvaton.org>
2016-02-23 22:40:23 +0100

    fixed a bug in latex escaping

--- src/lib/escape.rs
+++ src/lib/escape.rs
@@ -42,7 +42,7 @@ pub fn escape_tex(input: &str) -> String {
             '_' => output.push_str(r"\_"),
             '{' => output.push_str(r"\{"),
             '}' => output.push_str(r"\}"),
-            '~' => output.push_str(r"\textasciitilde" ),
+            '~' => output.push_str(r"\textasciitilde " ),
             '^' => output.push_str(r"\textasciicircum "),
             '\\' => output.push_str(r"\textbackslash "),
             _  => output.push(c)
