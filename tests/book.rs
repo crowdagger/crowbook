722e80ec356c856a8bfe79b862b103a268b37121
Elisabeth Henry <liz.henry@ouvaton.org>
2016-02-26 17:41:51 +0100

    tests/book.rs: call render_epub instead of render_all so it doesn't fail if latex is not present

--- tests/book.rs
+++ tests/book.rs
@@ -5,11 +5,11 @@ use std::env;
 #[test]
 fn test_book() {
     let book = Book::new_from_file(&format!("{}/{}", env!("CARGO_MANIFEST_DIR"), "tests/test.book"), false).unwrap();
-    book.render_all().unwrap();
+    book.render_epub().unwrap();
 }
 
 #[test]
 fn book_example() {
     let book = Book::new_from_file(&format!("{}/{}", env!("CARGO_MANIFEST_DIR"), "book_example/config.book"), false).unwrap();
-    book.render_all().unwrap();
+    book.render_epub().unwrap();
 }
