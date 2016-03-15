cca4d9f5ca4f63dc3e37ffc6df31348c0ecb8dd9
Elisabeth Henry <liz.henry@ouvaton.org>
2016-03-15 18:13:56 +0100

    Make javascript find correct chapter to display according to #anchor

--- templates/script.js
+++ templates/script.js
@@ -1,32 +1,40 @@
 /* Builtin script also includes html_dir.script */
 {{#display_chapter}}
-function showChapter(chap,noreset){
+function showChapter(chap, noreset){
     if (!displayAll) {
         var chapters = document.getElementsByClassName("chapter");
         for (i = 0; i < chapters.length; i++) {
-            if (i == chap){
+            if (i == chap) {
                 chapters[i].style.display = "block";
-            }else{
+            } else {
                 chapters[i].style.display = "none";
             }
         }
         var controls = document.getElementsByClassName("chapterControls");
         for (i = 0; i < controls.length; i++){
-            if (i>=chap*2-1 && i<=chap*2){
+            if (i >= chap * 2-1 && i <= chap * 2) {
                 controls[i].style.display = "block";
-            }else{
+            } else {
                 controls[i].style.display = "none";
             }
         }
         if (!noreset) {
             window.location.hash = "#chapter-"+chap;
         }
-    }else{
+    } else {
         window.location.hash = "#chapter-"+chap;
     }
 }
 
-function switchAll(){
+function getChapter(elem) {
+    if (elem.className == "chapter") {
+        return parseInt(elem.id.substr("chapter-".length));
+    } else {
+        return getChapter(elem.parentElement);
+    }
+}
+
+function switchAll() {
     if (!displayAll){
         displayAll = true;
         var chapters = document.getElementsByClassName("chapter");
@@ -49,6 +57,13 @@ function switchAll(){
 
 window.onload = function(){
     displayAll = false;
-    showChapter(0,true);
+    var hash = document.location.hash;
+    if (!hash) {
+        showChapter(0, true);
+    } else {
+        var element = document.getElementById(hash.substr(1));
+        var chap = getChapter(element);
+        showChapter(chap, true);
+    }
 };
 {{/display_chapter}}
