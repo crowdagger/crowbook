6ba07c38d5cfa2da741f4a2604bd781498bee279
Elisabeth Henry <liz.henry@ouvaton.org>
2016-04-15 14:38:48 +0200

    Update cleaner test

--- src/lib/tests/cleaner.rs
+++ src/lib/tests/cleaner.rs
@@ -12,8 +12,8 @@ fn cleaner_default() {
 #[test]
 fn cleaner_french() {
     let mut res = String::from("  «  Comment allez-vous ? » demanda-t-elle à son   interlocutrice  qui lui répondit  : « Mais très bien ma chère  !  »");
-    let french = French::new('~');
+    let french = French;
     french.clean(&mut res);
     println!("{}", &res);
-    test_eq(&res, " « Comment allez-vous~? » demanda-t-elle à son interlocutrice qui lui répondit : « Mais très bien ma chère~! »");
+    test_eq(&res, " « Comment allez-vous ? » demanda-t-elle à son interlocutrice qui lui répondit : « Mais très bien ma chère ! »");
 }
