0b7eaa96d0ef9de62580a7b11918866be773b942
Elisabeth Henry <liz.henry@ouvaton.org>
2016-09-18 02:47:12 +0200

    Add licence information

--- src/lib/html_dir.rs
+++ src/lib/html_dir.rs
@@ -1,3 +1,20 @@
+// Copyright (C) 2016 Élisabeth HENRY.
+//
+// This file is part of Crowbook.
+//
+// Crowbook is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 2.1 of the License, or
+// (at your option) any later version.
+//
+// Caribon is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received ba copy of the GNU Lesser General Public License
+// along with Crowbook.  If not, see <http://www.gnu.org/licenses/>.
+
 use error::{Error,Result, Source};
 use html::HtmlRenderer;
 use book::Book;
