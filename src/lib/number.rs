2c3b2752633a70a7a15fbb6a4dfcaa8626750e9e
Elisabeth Henry <liz.henry@ouvaton.org>
2016-09-18 02:32:34 +0200

    Add is_hidden and is_numbered methods to Number

--- src/lib/number.rs
+++ src/lib/number.rs
@@ -10,3 +10,19 @@ pub enum Number {
     /// chapter number set to specified number
     Specified(i32), 
 }
+
+
+impl Number {
+    /// Returns true if number is hidden
+    pub fn is_hidden(&self) -> bool {
+        *self == Number::Hidden
+    }
+
+    /// Returns true if number is numbered
+    pub fn is_numbered(&self) -> bool {
+        match *self {
+            Hidden | Unnumbered => false,
+            _ => true,
+        }
+    }
+}
