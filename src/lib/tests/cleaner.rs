0917a0be9d7baafee0860d61c6d3771260679139
Elisabeth Henry <liz.henry@ouvaton.org>
2016-09-19 21:58:55 +0200

    Update cleaner tests

--- src/lib/tests/cleaner.rs
+++ src/lib/tests/cleaner.rs
@@ -1,19 +1,18 @@
 use cleaner::{French, Cleaner, Default};
 use super::test_eq;
+use std::borrow::Cow;
 
 #[test]
 fn cleaner_default() {
-    let mut res = String::from("   Remove    supplementary   spaces    but    don't    trim     either   ");
-    Default.clean(&mut res, false);
+    let s = Cow::Borrowed("   Remove    supplementary   spaces    but    don't    trim     either   ");
+    let res = Default.clean(s, false);
     test_eq(&res, " Remove supplementary spaces but don't trim either ");
 }
 
 
 #[test]
 fn cleaner_french() {
-    let mut res = String::from("  «  Comment allez-vous ? » demanda-t-elle à son   interlocutrice  qui lui répondit  : « Mais très bien ma chère  !  »");
-    let french = French;
-    french.clean(&mut res, false);
-    println!("{}", &res);
+    let s = Cow::Borrowed("  «  Comment allez-vous ? » demanda-t-elle à son   interlocutrice  qui lui répondit  : « Mais très bien ma chère  !  »");
+    let res = French.clean(s, false);
     test_eq(&res, " « Comment allez-vous ? » demanda-t-elle à son interlocutrice qui lui répondit : « Mais très bien ma chère ! »");
 }
