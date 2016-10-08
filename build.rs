8f082c06485aeb3abfedbbad65e160a43fb48b15
Elisabeth Henry <liz.henry@ouvaton.org>
2016-10-08 14:52:35 +0200

    Add rerun-if-changed lines in build.rs

--- build.rs
+++ build.rs
@@ -2,6 +2,8 @@ extern crate crowbook_localize;
 use crowbook_localize::Localizer;
 
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=lang/fr.mo");
     let mut localizer = Localizer::new();
     localizer.add_lang("fr", include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/lang/fr.mo"))).unwrap();
     localizer.write_macro_file(concat!(env!("CARGO_MANIFEST_DIR"), "/src/lib/localize_macros.rs")).unwrap();
