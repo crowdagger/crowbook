6fc6f0cb277426dccde1c639f8e32320b0fcc29c
Elisabeth Henry <liz.henry@ouvaton.org>
2016-10-26 16:11:09 +0200

    Use env::arg instead of env macro

--- build.rs
+++ build.rs
@@ -2,16 +2,20 @@ extern crate crowbook_localize;
 use crowbook_localize::{Localizer, Extractor};
 
 use std::path::Path;
+use std::env;
+
 
 fn main() {
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=lang/fr.po");
+//    println!("cargo:rerun-if-changed=build.rs");
+//    println!("cargo:rerun-if-changed=lang/fr.po");
     let mut extractor = Extractor::new();
     extractor.add_messages_from_dir(concat!(env!("CARGO_MANIFEST_DIR"), "/src")).unwrap();
     extractor.write_pot_file(concat!(env!("CARGO_MANIFEST_DIR"), "/lang/crowbook.pot")).unwrap();
     
     let mut localizer = Localizer::new(&extractor);
     localizer.add_lang("fr", include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/lang/fr.po"))).unwrap();
-    let dest_path = Path::new(env!("OUT_DIR")).join("localize_macros.rs");
+    let out_dir_path = env::var("OUT_DIR").unwrap();
+    let out_dir = Path::new(&out_dir_path);
+    let dest_path = out_dir.join("localize_macros.rs");
     localizer.write_macro_file(dest_path.to_str().unwrap()).unwrap();
 }
