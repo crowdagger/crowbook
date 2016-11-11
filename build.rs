c0b863e62873ae7c6aaa50c6ad15eed5e68aec31
Elisabeth Henry <liz.henry@ouvaton.org>
2016-11-11 15:31:36 +0100

    Make build.rs more concise

--- build.rs
+++ build.rs
@@ -14,8 +14,7 @@ fn main() {
     
     let mut localizer = Localizer::new(&extractor);
     localizer.add_lang("fr", include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/lang/fr.po"))).unwrap();
-    let out_dir_path = env::var("OUT_DIR").unwrap();
-    let out_dir = Path::new(&out_dir_path);
-    let dest_path = out_dir.join("localize_macros.rs");
-    localizer.write_macro_file(dest_path.to_str().unwrap()).unwrap();
+    let dest_path = Path::new(&env::var("OUT_DIR").unwrap())
+        .join("localize_macros.rs");
+    localizer.write_macro_file(dest_path).unwrap();
 }
