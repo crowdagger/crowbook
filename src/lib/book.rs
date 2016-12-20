e63a15f64dad7de778de1304b0aa6e1af91dcac1
Elisabeth Henry <liz.henry@ouvaton.org>
2016-12-20 16:07:15 +0100

    Add doc example to Book::set_options

--- src/lib/book.rs
+++ src/lib/book.rs
@@ -145,6 +145,16 @@ impl Book {
     ///
     /// # Arguments
     /// *`options` a list (or other iterator) of (key, value) tuples. Can be &[].
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use crowbook::Book;
+    /// let mut book = Book::new();
+    /// book.set_options(&[("author", "Foo"), ("title", "Bar")]);
+    /// assert_eq!(book.options.get_str("title").unwrap(), "Bar");
+    /// assert_eq!(book.options.get_str("author").unwrap(), "Foo");
+    /// ```
     pub fn set_options<'a, I>(&mut self, options: I) -> &mut Book
         where I: IntoIterator<Item = &'a (&'a str, &'a str)>
     {
