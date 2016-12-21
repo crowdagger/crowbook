12530e836d66116b043f6e72a83bd9c5bf993a4d
Elisabeth Henry <liz.henry@ouvaton.org>
2016-12-21 22:39:35 +0100

    Refactor toc implementation

--- src/lib/tests/toc.rs
+++ src/lib/tests/toc.rs
@@ -11,25 +11,17 @@ fn toc_simple() {
     toc.add(1, String::from("#5"), "2".to_owned());
     let actual = toc.render();
     let expected = "<ul>
- <ul>
-  <ul>
-   <li><a href = \"#1\">0.0.1</a>
-   </li>
-  </ul>
- </ul>
- <li><a href = \"#2\">1</a>
- <ul>
-  <ul>
-   <li><a href = \"#3\">1.0.1</a>
-   </li>
-  </ul>
-  <li><a href = \"#4\">1.1</a>
-  </li>
- </ul>
- </li>
- <li><a href = \"#5\">2</a>
- </li>
-</ul>";
+<li><a href = \"#1\">0.0.1</a></li>
+<li><a href = \"#2\">1</a>
+<ul><li><a href = \"#3\">1.0.1</a></li>
+<li><a href = \"#4\">1.1</a></li>
+
+</ul>
+</li>
+<li><a href = \"#5\">2</a></li>
+
+</ul>
+";
     test_eq(&actual, expected);
 }
 
@@ -40,20 +32,20 @@ fn toc_epub_simple() {
     toc.add(1, String::from("#2"), "2".to_owned());
     let actual = toc.render_epub(1);
     let expected = "
-   <navPoint id = \"navPoint-1\">
-     <navLabel>
-       <text>1</text>
-     </navLabel>
-     <content src = \"#1\" />
-    </navPoint>
+<navPoint id = \"navPoint-1\">
+  <navLabel>
+   <text>1</text>
+  </navLabel>
+  <content src = \"#1\" />
 
-   <navPoint id = \"navPoint-2\">
-     <navLabel>
-       <text>2</text>
-     </navLabel>
-     <content src = \"#2\" />
-    </navPoint>
-";
+</navPoint>
+<navPoint id = \"navPoint-2\">
+  <navLabel>
+   <text>2</text>
+  </navLabel>
+  <content src = \"#2\" />
+
+</navPoint>";
     test_eq(&actual, expected);
 }
 
@@ -66,34 +58,34 @@ fn toc_epub_simple_sublevels() {
     toc.add(2, String::from("#2.1"), "2.1".to_owned());
     let actual = toc.render_epub(1);
     let expected = "
-   <navPoint id = \"navPoint-1\">
-     <navLabel>
-       <text>1</text>
-     </navLabel>
-     <content src = \"#1\" />
+<navPoint id = \"navPoint-1\">
+  <navLabel>
+   <text>1</text>
+  </navLabel>
+  <content src = \"#1\" />
 
-   <navPoint id = \"navPoint-2\">
-     <navLabel>
-       <text>1.1</text>
-     </navLabel>
-     <content src = \"#1.1\" />
-    </navPoint>
-    </navPoint>
+<navPoint id = \"navPoint-2\">
+  <navLabel>
+   <text>1.1</text>
+  </navLabel>
+  <content src = \"#1.1\" />
 
-   <navPoint id = \"navPoint-3\">
-     <navLabel>
-       <text>2</text>
-     </navLabel>
-     <content src = \"#2\" />
+</navPoint>
+</navPoint>
+<navPoint id = \"navPoint-3\">
+  <navLabel>
+   <text>2</text>
+  </navLabel>
+  <content src = \"#2\" />
 
-   <navPoint id = \"navPoint-4\">
-     <navLabel>
-       <text>2.1</text>
-     </navLabel>
-     <content src = \"#2.1\" />
-    </navPoint>
-    </navPoint>
-";
+<navPoint id = \"navPoint-4\">
+  <navLabel>
+   <text>2.1</text>
+  </navLabel>
+  <content src = \"#2.1\" />
+
+</navPoint>
+</navPoint>";
     test_eq(&actual, expected);
 }
 
@@ -106,26 +98,24 @@ fn toc_epub_broken_sublevels() {
     toc.add(2, String::from("#2.1"), "2.1".to_owned());
     let actual = toc.render_epub(1);
     let expected = "
-   <navPoint id = \"navPoint-1\">
-     <navLabel>
-       <text>1.1</text>
-     </navLabel>
-     <content src = \"#1.1\" />
-    </navPoint>
+<navPoint id = \"navPoint-1\">
+  <navLabel>
+   <text>1.1</text>
+  </navLabel>
+  <content src = \"#1.1\" />
 
-   <navPoint id = \"navPoint-2\">
-     <navLabel>
-       <text>2</text>
-     </navLabel>
-     <content src = \"#2\" />
+</navPoint>\n<navPoint id = \"navPoint-2\">
+  <navLabel>
+   <text>2</text>
+  </navLabel>
+  <content src = \"#2\" />
 
-   <navPoint id = \"navPoint-3\">
-     <navLabel>
-       <text>2.1</text>
-     </navLabel>
-     <content src = \"#2.1\" />
-    </navPoint>
-    </navPoint>
-";
+<navPoint id = \"navPoint-3\">
+  <navLabel>
+   <text>2.1</text>
+  </navLabel>
+  <content src = \"#2.1\" />
+
+</navPoint>\n</navPoint>";
     test_eq(&actual, expected);
 }
--- src/lib/toc.rs
+++ src/lib/toc.rs
@@ -35,146 +35,50 @@ impl Toc {
     /// Adds an element
     pub fn add(&mut self, level: i32, url: String, title: String) {
         let element = TocElement::new(level, url, title);
-        self.elements.push(element);
+        let mut inserted = false;
+        if let Some(ref mut last_elem) = self.elements.last_mut() {
+            if level > last_elem.level {
+                last_elem.add(element.clone());
+                inserted = true;
+            }
+        }
+        if !inserted {
+            self.elements.push(element);
+        }
     }
 
     /// Render the Toc in a toc.ncx compatible way, for EPUB.
     pub fn render_epub(&mut self, mut offset: u32) -> String {
-        self.parts_hack();
         let mut output = String::new();
-        let mut levels = vec![];
-        
-        for element in self.elements.iter() {
-            let mut last_level;
-            
-            loop {
-                last_level = if levels.is_empty() {
-                    0
-                } else {
-                    levels[levels.len() - 1]
-                };
-
-                if last_level == element.level {
-                    output.push_str("    </navPoint>\n");
-                    break;
-                } else if element.level >= last_level  {
-                    levels.push(element.level);
-                    break;
-                } else /* if element.level < last_level */ {
-                    levels.pop().unwrap();
-                    output.push_str("    </navPoint>\n");
-                    continue;
-                }
-            }
-            
-            output.push_str(&format!("
-   <navPoint id = \"navPoint-{id}\">
-     <navLabel>
-       <text>{title}</text>
-     </navLabel>
-     <content src = \"{url}\" />
-",
-                                   id = offset,
-                                   title = element.title,
-                                   url = element.url));
-            offset += 1;
-        }
-        for _ in levels {
-            output.push_str("    </navPoint>\n");
+        let mut offset = 0;
+        for elem in &self.elements {
+            let mut i = 0;
+            let (n, s) = elem.render_epub(offset);
+            offset = n;
+            output.push_str(&s);
         }
         output
-    }
-
-    /// Handle parts (sort of) by adding 1 to all levels if lowest level is zero
-    fn parts_hack(&mut self) {
-        let mut contains_zero = false;
-        for elem in &self.elements {
-            if elem.level == 0 {
-                contains_zero = true;
-                break;
-            }
-        }
-        if contains_zero {
-            for mut elem in &mut self.elements {
-                elem.level += 1;
-            }
-        }
     }
 
     /// Render the Toc in either <ul> or <ol> form (according to Self::numbered
     pub fn render(&mut self) -> String {
-        self.parts_hack();
-        
         let mut output = String::new();
-
-        let mut x = 0;
-        let mut level = 0;
-        output.push_str(&self.render_vec(&mut x, &mut level));
-        for i in (0..level).rev() {
-            output.push_str(&format!("{}</{}>",
-                                     iter::repeat(' ').take(i as usize).collect::<String>(),
-                                     if self.numbered { "ol" } else { "ul" }));
+        for elem in &self.elements {
+            output.push_str(&elem.render(self.numbered));
         }
-        output
-    }
-
-    fn render_vec(&self, x: &mut usize, level: &mut i32) -> String {
-        let orig_level = *level;
-        let mut content = String::new();
-        while *x < self.elements.len() {
-            let elem = &self.elements[*x];
-
-            if elem.level <= orig_level {
-                return content;
-            }
-
-            *x += 1;
-
-            if elem.level > *level {
-                for i in *level..elem.level {
-                    content.push_str(&format!("{}{}\n",
-                                              iter::repeat(' ')
-                                                  .take(i as usize)
-                                                  .collect::<String>(),
-                                              if self.numbered { "<ol>" } else { "<ul>" }));
-                    *level = elem.level;
-                }
-            } else if elem.level < *level {
-                for i in (elem.level..*level).rev() {
-                    content.push_str(&format!("{}</{}>\n",
-                                              iter::repeat(' ')
-                                                  .take(i as usize)
-                                                  .collect::<String>(),
-                                              if self.numbered { "ol" } else { "ul" }));
-                }
-                *level = elem.level;
-            }
-            let spaces: String = iter::repeat(' ').take(elem.level as usize).collect();
-            content.push_str(&format!("{}<li><a href = \"{}\">{}</a>\n",
-                                      spaces,
-                                      elem.url,
-                                      elem.title));
-            content.push_str(&self.render_vec(x, level));
-
-            for i in (elem.level..*level).rev() {
-                content.push_str(&format!("{}</{}>\n",
-                                          iter::repeat(' ').take(i as usize).collect::<String>(),
-                                          if self.numbered { "ol" } else { "ul" }));
-            }
-            *level = elem.level;
-            content.push_str(&format!("{}</li>\n", spaces));
-
-        }
-        content
+        format!("<{oul}>\n{output}\n</{oul}>\n",
+                output = output,
+                oul = if self.numbered { "ol" } else { "ul" })
     }
 }
 
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct TocElement {
     level: i32,
     url: String,
     title: String,
+    children: Vec<TocElement>,
 }
 
 impl TocElement {
@@ -183,7 +87,73 @@ impl TocElement {
             level: level,
             url: url,
             title: title,
+            children: vec!(),
         }
+    }
+
+    // Add element to self or to children, according to level
+    pub fn add(&mut self, element: TocElement) {
+        let mut inserted = false;
+        if let Some(ref mut last_elem) = self.children.last_mut() {
+            if element.level > last_elem.level {
+                last_elem.add(element.clone());
+                inserted = true;
+            }
+        }
+        if !inserted {
+            self.children.push(element);
+        }
+    }
+
+    // Render element for Epub's toc.ncx format
+    pub fn render_epub(&self, mut offset: u32) -> (u32, String) {
+        offset += 1;
+        let id = offset;
+        let children = if self.children.is_empty() {
+            String::new()
+        } else {
+            let mut output = String::new();
+            for child in &self.children {
+                let (n, s) = child.render_epub(offset);
+                offset = n;
+                output.push_str(&s);
+            }
+            output
+        };
+        (offset,
+         format!("
+<navPoint id = \"navPoint-{id}\">
+  <navLabel>
+   <text>{title}</text>
+  </navLabel>
+  <content src = \"{url}\" />
+{children}
+</navPoint>",
+                id = id,
+                title = self.title,
+                url = self.url,
+                children = children))
+                
+    }
+
+    // Render element as a list element
+    pub fn render(&self, numbered: bool) -> String {
+        let children = if self.children.is_empty() {
+            String::new()
+        } else {
+            let mut output = String::new();
+            for child in &self.children {
+                output.push_str(&child.render(numbered));
+            }
+            format!("\n<{oul}>{children}\n</{oul}>\n",
+                    oul = if numbered { "ol" } else { "ul" },
+                    children = output)
+        };
+        format!("<li><a href = \"{link}\">{title}</a>{children}</li>\n",
+                link = self.url,
+                title = self.title,
+                children = children)
+
     }
 }
 
