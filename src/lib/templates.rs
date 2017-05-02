e75820e3808cb9cfad8df292b26d6b850606d1be
Elisabeth Henry <liz.henry@ouvaton.org>
2017-05-02 05:59:43 +0200

    Remove lifetime annotation (no longer needed in rustc 1.17.0)

--- src/lib/templates.rs
+++ src/lib/templates.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2016 Élisabeth HENRY.
+// Copyright (C) 2016, 2017 Élisabeth HENRY.
 //
 // This file is part of Crowbook.
 //
@@ -16,52 +16,52 @@
 // along with Crowbook.  If not, see <http://www.gnu.org/licenses/>.
 
 pub mod highlight {
-    pub static CSS: &'static str = include_str!("../../templates/highlight/default.css");
-    pub static JS: &'static str = include_str!("../../templates/highlight/highlight.pack.js");
+    pub static CSS: &str = include_str!("../../templates/highlight/default.css");
+    pub static JS: &str = include_str!("../../templates/highlight/highlight.pack.js");
 }
 
 pub mod html {
-    pub static CSS: &'static str = concat!(include_str!("../../templates/epub/stylesheet.css"),
+    pub static CSS: &str = concat!(include_str!("../../templates/epub/stylesheet.css"),
                                            include_str!("../../templates/html/template.css"));
-    pub static CSS_COLOURS: &'static str = include_str!("../../templates/html/colours.css");
-    pub static PRINT_CSS: &'static str = include_str!("../../templates/html/print.css");
-    pub static JS: &'static str = include_str!("../../templates/html/script.js");
+    pub static CSS_COLOURS: &str = include_str!("../../templates/html/colours.css");
+    pub static PRINT_CSS: &str = include_str!("../../templates/html/print.css");
+    pub static JS: &str = include_str!("../../templates/html/script.js");
 
 }
 
 pub mod img {
-    pub static MENU_SVG: &'static [u8] = include_bytes!("../../templates/img/menu.svg");
-    pub static BOOK_SVG: &'static [u8] = include_bytes!("../../templates/img/book.svg");
-    pub static PAGES_SVG: &'static [u8] = include_bytes!("../../templates/img/pages.svg");
+    pub static MENU_SVG: &[u8] = include_bytes!("../../templates/img/menu.svg");
+    pub static BOOK_SVG: &[u8] = include_bytes!("../../templates/img/book.svg");
+    pub static PAGES_SVG: &[u8] = include_bytes!("../../templates/img/pages.svg");
 }
 
 pub mod html_single {
-    pub static HTML: &'static str = include_str!("../../templates/html_single/template.html");
-    pub static JS: &'static str = include_str!("../../templates/html_single/script.js");
+    pub static HTML: &str = include_str!("../../templates/html_single/template.html");
+    pub static JS: &str = include_str!("../../templates/html_single/script.js");
 }
 
 pub mod html_dir {
-    pub static TEMPLATE: &'static str = include_str!("../../templates/html_dir/template.html");
+    pub static TEMPLATE: &str = include_str!("../../templates/html_dir/template.html");
 }
 
 pub mod latex {
-    pub static TEMPLATE: &'static str = include_str!("../../templates/latex/template.tex");
+    pub static TEMPLATE: &str = include_str!("../../templates/latex/template.tex");
 }
 
 pub mod epub {
-    pub static TEMPLATE: &'static str = include_str!("../../templates/epub/template.xhtml");
-    pub static CSS: &'static str = include_str!("../../templates/epub/stylesheet.css");
-    pub static COVER: &'static str = include_str!("../../templates/epub/cover.xhtml");
-    pub static TITLE: &'static str = include_str!("../../templates/epub/titlepage.xhtml");
+    pub static TEMPLATE: &str = include_str!("../../templates/epub/template.xhtml");
+    pub static CSS: &str = include_str!("../../templates/epub/stylesheet.css");
+    pub static COVER: &str = include_str!("../../templates/epub/cover.xhtml");
+    pub static TITLE: &str = include_str!("../../templates/epub/titlepage.xhtml");
 }
 
 pub mod epub3 {
-    pub static TEMPLATE: &'static str = include_str!("../../templates/epub3/template.xhtml");
-    pub static COVER: &'static str = include_str!("../../templates/epub3/cover.xhtml");
-    pub static TITLE: &'static str = include_str!("../../templates/epub3/titlepage.xhtml");
+    pub static TEMPLATE: &str = include_str!("../../templates/epub3/template.xhtml");
+    pub static COVER: &str = include_str!("../../templates/epub3/cover.xhtml");
+    pub static TITLE: &str = include_str!("../../templates/epub3/titlepage.xhtml");
 }
 
 pub mod odt {
-    pub static CONTENT: &'static str = include_str!("../../templates/odt/content.xml");
-    pub static ODT: &'static [u8] = include_bytes!("../../templates/odt/template.odt");
+    pub static CONTENT: &str = include_str!("../../templates/odt/content.xml");
+    pub static ODT: &[u8] = include_bytes!("../../templates/odt/template.odt");
 }
